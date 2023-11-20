86732a0b01509688e87a9015d882f22a722b4bdc
Hasan Khan <115020762+HasanK404@users.noreply.github.com>
2023-11-20 02:07:19 -0500

    commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send ('<h1>${Date()}</h1>')
+})
+
 app.use(express.static("public"))
 
 const port = 8080
